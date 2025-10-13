114462ea6e7cfad4f621a76e43533f152a1cd6f8
mplorentz <mplorentz@users.noreply.github.com>
2025-10-13 15:55:03 -0400

    testing golden test failure

--- lib/screens/lockbox_list_screen.dart
+++ lib/screens/lockbox_list_screen.dart
@@ -106,7 +106,7 @@ class LockboxListScreen extends ConsumerWidget {
                             ),
                             const SizedBox(height: 16),
                             Text(
-                              'No lockboxes yet',
+                              'boop beep',
                               style: textTheme.titleLarge,
                             ),
                             const SizedBox(height: 8),
