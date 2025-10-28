059beef36b79d460f1ecb4fc381e4f2ee23ec068
mplorentz <mplorentz@users.noreply.github.com>
2025-10-28 13:34:30 -0400

    commit rest of todo?

--- lib/providers/recovery_provider.dart
+++ lib/providers/recovery_provider.dart
@@ -62,7 +62,8 @@ class RecoveryStatus {
 
 /// Provider for a specific recovery request by ID
 /// This watches the lockbox stream and extracts the recovery request, so it updates automatically
-/// TODO: This should probably be a StreamProvider
+// TODO: This should probably be a StreamProvider? I don't really understand the point of
+// providers that don't live update.
 final recoveryRequestByIdProvider =
     Provider.family<AsyncValue<RecoveryRequest?>, String>((ref, recoveryRequestId) {
   // We need to find which lockbox contains this recovery request
